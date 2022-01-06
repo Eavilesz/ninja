16a73d3edb85bda60e3baf53426e35319a084729
Eavilesz <ernesto-av@hotmail.com>
2022-01-06 17:25:27 -0500

    [TAREA] sensei

--- ninja.js
+++ ninja.js
@@ -10,7 +10,7 @@ class Ninja {
   }
   showStats() {
     console.log(
-      `nombre: ${this.nombre}, salud:${this.salud}, velocidad:${this.velocidad} y fuerza:${this.fuerza}.`
+      `Nombre: ${this.nombre}, salud:${this.salud}, velocidad:${this.velocidad} y fuerza:${this.fuerza}.`
     );
   }
   drinkSake() {
@@ -18,8 +18,27 @@ class Ninja {
   }
 }
 
-const ninja1 = new Ninja("Hyabusa", 10);
+class Sensei extends Ninja {
+  constructor(nombre) {
+    super(nombre, 200);
+    this.velocidad = 10;
+    this.fuerza = 10;
+    this.sabiduria = 10;
+  }
+  speakWisdom() {
+    super.drinkSake();
+    console.log(
+      "Lo que un programador puede hacer en un mes, dos programadores pueden hacerlo en dos meses."
+    );
+  }
+}
+
+const ninja1 = new Ninja("Hyabusa", 0);
 ninja1.sayName();
 ninja1.showStats();
 ninja1.drinkSake();
 console.log(ninja1.salud);
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.speakWisdom();
+superSensei.showStats();
